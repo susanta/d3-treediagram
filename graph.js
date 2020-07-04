ddf79acceac239cc781ccdcf6eb6f8f5e7b875f7
Susanta <inbox.susanta@gmail.com>
2020-07-04 13:53:35 +0530

    Add ordinal scale

--- graph.js
+++ graph.js
@@ -16,11 +16,17 @@ const stratify = d3
 
 const tree = d3.tree().size([dims.width, dims.height]);
 
+// create ordinal scale
+const colour = d3.scaleOrdinal(['#f4511e', '#e91e63', '#e53935', '#9c27b0']);
+
 // update function
 const update = (data) => {
   // remove current nodes
   graph.selectAll('.node').remove();
   graph.selectAll('.link').remove();
+
+  // update ordinal scale domain
+  colour.domain(data.map((d) => d.department));
 
   // get updated root node data
   const rootNode = stratify(data);
@@ -62,7 +68,8 @@ const update = (data) => {
   // appends rects to enter nodes
   enterNodes
     .append('rect')
-    .attr('fill', '#aaa')
+    // apply the ordinal scale for fill
+    .attr('fill', (d) => colour(d.data.department))
     .attr('stroke', '#555')
     .attr('stroke-width', 2)
     .attr('height', 50)
